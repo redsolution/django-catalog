c311ca1b6bcc09d1cca11d7b388af4b7bc17254f
Ivan Gromov <ivan.gromov@redsolution.ru>
2009-08-18 18:12:35 +0600

    Added full media package

--- catalog/context_processors.py
+++
@@ -1,7 +0,0 @@
-from catalog import settings
-
-def media(request):
-    """
-    Adds media-related context variables to the context.
-    """
-    return {'CATALOG_MEDIA_URL': settings.CATALOG_MEDIA_URL}
