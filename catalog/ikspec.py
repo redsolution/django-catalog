24eae5cbe82cf84d368731da16fafb861f88ba23
Ivan Gromov <ivan.gromov@redsolution.ru>
2009-08-24 12:21:00 +0600

    Changed default image sizes

--- catalog/ikspec.py
+++ catalog/ikspec.py
@@ -2,15 +2,15 @@ from imagekit import processors
 from imagekit.specs import ImageSpec 
 
 class ResizeDisplay(processors.Resize):
-    width = 200
+    width = 300
     height = 300
 
 class Display(ImageSpec):
     processors = [ResizeDisplay, ]
 
 class ThumbDisplay(processors.Resize):
-    width = 130
-    height = 130
+    width = 180
+    height = 180
 
 class Thumb(ImageSpec):
     pre_cache = True 
