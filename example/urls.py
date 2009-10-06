47cf5ab852c365764a210e371e6f5b4a50fc74dd
Ivan Gromov <ivan.gromov@redsolution.ru>
2009-10-06 12:55:52 +0600

    Added example project

---
+++ example/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls.defaults import *
+from django.contrib import admin
+from django.conf import settings
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^admin/catalog/', include('catalog.admin.urls')),
+    (r'^admin/(.*)', admin.site.root),
+    url(r'^', include('catalog.urls')),
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+         {'document_root': settings.MEDIA_ROOT}),
+    )
