a235b6ddbd67c717272c0803c70f2d5cf5608729
Ivan Gromov <ivan.gromov@redsolution.ru>
2009-10-26 10:19:13 +0500

    Changed decorator for admin

--- catalog/admin/urls.py
+++ catalog/admin/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls.defaults import patterns, url
 from django.views.generic.simple import direct_to_template
-from django.contrib.auth.decorators import login_required
 from catalog.admin.ext import ext_site
+from catalog.admin.utils import admin_permission_required
 
 
 urlpatterns = patterns('catalog.admin',
-    url(r'^$', login_required(direct_to_template), {'template': 'admin/catalog/main.html'}),
-    url(r'^/closepopup$', login_required(direct_to_template), {'template': 'admin/catalog/closepopup.html'}),
+    url(r'^$', admin_permission_required('caatalog.tree_item')(direct_to_template),
+        {'template': 'admin/catalog/main.html'}),
+    url(r'^/closepopup$', admin_permission_required('catalog.tree_item')(direct_to_template),
+        {'template': 'admin/catalog/closepopup.html'}),
 
     url(r'^new/(?P<model_name>\w+)/$', 'views.add_instance'),
     url(r'^edititem/(\d{1,7})/$', 'views.editor_redirect'),
