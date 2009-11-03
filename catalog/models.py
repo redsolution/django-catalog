cebe925e1349ef9d094afe7233c58fff993684f7
Ivan Gromov <ivan.gromov@redsolution.ru>
2009-11-03 18:16:12 +0500

    Removed unused decorator

--- catalog/models.py
+++ catalog/models.py
@@ -76,7 +76,6 @@ class TreeItem(models.Model):
     
     manager = TreeItemManager()
 
-    @models.permalink
     def get_absolute_url(self):
         return self.get_absolute_url_undecorated()
     
