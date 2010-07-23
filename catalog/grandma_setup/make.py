f5213ce1deda2025c5d89fa0c53851d96861f0ed
Ivan Gromov <ivan.gromov@redsolution.ru>
2010-07-23 17:16:04 +0600

    Added django-server-config support

--- catalog/grandma_setup/make.py
+++ catalog/grandma_setup/make.py
@@ -2,6 +2,7 @@ from grandma.make import BaseMake
 from grandma.models import GrandmaSettings
 from catalog.grandma_setup.models import CatalogSettings
 from os.path import dirname, join
+import shutil
 
 
 class Make(BaseMake):
@@ -19,5 +20,34 @@ class Make(BaseMake):
         self.grandma_settings.render_to(['..', 'templates', 'admin', 'index.html'],
             'catalog/grandma/admin_index.html', {
             'catalog_settings': catalog_settings,
-        })
+        }, 'w')
 
+
+    def postmake(self):
+        super(Make, self).postmake()
+        if 'grandma.django-server-config' not in self.grandma_settings.installed_packages:
+            catalog_media_dir = join(dirname(dirname(__file__)), 'media')
+            project_media_dir = join(self.grandma_settings.project_dir, 'media')
+
+#           WARNING! Silently delete media dirs
+            try:
+                shutil.rmtree(join(project_media_dir, 'extjs'))
+#            no such directory
+            except OSError:
+                pass
+            try:
+                shutil.rmtree(join(project_media_dir, 'catalog'))
+            except OSError:
+                pass
+#           copy files to media directory
+            shutil.copytree(
+                join(catalog_media_dir, 'catalog'),
+                join(project_media_dir, 'catalog'),
+            )
+            shutil.copytree(
+                join(catalog_media_dir, 'extjs'),
+                join(project_media_dir, 'extjs'),
+            )
+
+#        if 'grandma.django-menu-proxy' in self.grandma_settings.installed_packages:
+#            grandma_settings.render_to('settings.py', 'catalog/grandma/settings_menu.py')
