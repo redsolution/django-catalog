7fdea0087a5ce5fd87fb565392611ede1b6cccb3
Ivan Gromov <ivan.gromov@redsolution.ru>
2010-07-23 16:31:29 +0600

    Fixed CMS settings module

--- catalog/grandma_setup/make.py
+++ catalog/grandma_setup/make.py
@@ -1,19 +1,23 @@
 from grandma.make import BaseMake
 from grandma.models import GrandmaSettings
 from catalog.grandma_setup.models import CatalogSettings
+from os.path import dirname, join
+
 
 class Make(BaseMake):
+    grandma_settings = GrandmaSettings.objects.get_settings()
+
     def make(self):
         super(Make, self).make()
         catalog_settings = CatalogSettings.objects.get_settings()
-        grandma_settings = GrandmaSettings.objects.get_settings()
-        grandma_settings.render_to('settings.py', 'catalog/grandma/settings.py', {
+        self.grandma_settings.render_to('settings.py', 'catalog/grandma/settings.py', {
             'catalog_settings': catalog_settings,
         })
-        grandma_settings.render_to('urls.py', 'catalog/grandma/urls.py', {
+        self.grandma_settings.render_to('urls.py', 'catalog/grandma/urls.py', {
             'catalog_settings': catalog_settings,
         })
-        grandma_settings.render_to(['..', 'templates', 'admin', 'index.html'],
+        self.grandma_settings.render_to(['..', 'templates', 'admin', 'index.html'],
             'catalog/grandma/admin_index.html', {
             'catalog_settings': catalog_settings,
         })
+
