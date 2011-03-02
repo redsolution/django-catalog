5323b0ed9965992b345fdef7647ad6535f9a928d
Ivan Gromov <summer.is.gone@gmail.com>
2011-03-03 03:18:27 +0500

    Catalog looks like catalog. Dummy tree needs refactoring in python.

---
+++ catalog/media/catalog/catalog.js
@@ -0,0 +1,65 @@
+;var app={};
+
+Ext.onReady(function() {
+    Ext.QuickTips.init();
+    
+    app.store = new Ext.data.DirectStore({
+        storeId: 'DataStore',
+        directFn: Catalog.treeitem.objects,
+        autoLoad: true,
+        remoteSort: true,
+        root: 'records',
+        fields: ['id', 'content_type','object_id','parent','tree_id']
+    });
+    
+    app.grid = new Ext.grid.GridPanel({
+        store: app.store,
+        title: 'Grid!',
+        sm: new Ext.grid.RowSelectionModel({singleSelect:true}),
+        cm:  new Ext.grid.ColumnModel({
+            columns: [{
+                header: 'ID',
+                dataIndex: 'id'
+            },{
+                header: 'Content type',
+                dataIndex: 'content_type'
+            },{
+                header: 'object id',
+                dataIndex: 'object_id'
+            }]
+        })
+    });
+    
+    app.tree = new Ext.tree.TreePanel({
+        title: 'Tree!',
+        root: {
+            id: 'root',
+            text: 'Catalog'
+        },
+        loader: new Ext.tree.TreeLoader({
+            directFn: Catalog.treeitem.tree
+        }),
+        
+    });
+    
+    app.view = new Ext.Viewport({
+        layout: 'border',
+        items: [{
+            region: 'north',
+            xtype: 'panel',
+            height: 50
+        },{
+            region: 'center',
+            layout: 'fit',
+            margins: '15 5 5 5',
+            items: app.grid
+        },{
+            region: 'west',
+            width: 200,
+            margins: '15 5 5 5',
+            layout: 'fit',
+            items: app.tree
+        }]
+    });
+    Ext.DomHelper.overwrite('content', '', false);
+});
