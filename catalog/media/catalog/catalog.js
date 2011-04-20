68445fe1913a5e0b48d3148b094b93aa3965fb43
Ivan Gromov <summer.is.gone@gmail.com>
2011-04-20 16:48:40 +0600

    Added CSRF support

--- catalog/media/catalog/catalog.js
+++ catalog/media/catalog/catalog.js
@@ -1,5 +1,15 @@
 ;var app={};
 
+Ext.Ajax.on('beforerequest', function (conn, options) {
+	   if (!(/^http:.*/.test(options.url) || /^https:.*/.test(options.url))) {
+	     if (typeof(options.headers) == "undefined") {
+	       options.headers = {'X-CSRFToken': Ext.util.Cookies.get('csrftoken')};
+	     } else {
+	       options.headers.extend({'X-CSRFToken': Ext.util.Cookies.get('csrftoken')});
+	     }                        
+	   }
+	}, this);
+
 Ext.onReady(function() {
     Ext.QuickTips.init();
     
