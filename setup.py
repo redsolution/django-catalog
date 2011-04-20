fae924dc290a4446e0be4e52d164e47395b8b6a9
Ivan Gromov <summer.is.gone@gmail.com>
2011-04-20 16:34:00 +0600

    Added extdirect setup requirement

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: News/Diary',
     ],
     packages=find_packages(exclude=['example', 'example.*']),
-    install_requires=[],
+    install_requires=['extdirect.django'],
     include_package_data=True,
     zip_safe=False,
     long_description=read('README'),
