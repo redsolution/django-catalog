78a803dae57136f8f07acb9ea2ec8a47085f9814
Ivan Gromov <ivan.gromov@redsolution.ru>
2011-05-30 17:46:39 +0600

    Added docstrings to views

--- catalog/views.py
+++ catalog/views.py
@@ -8,10 +8,6 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext, loader
 from django.views.generic.list_detail import object_detail, object_list
 
-defaults = {
-    'template_name': 'catalog/treeitem.html'
-}
-
 def catalog(request):
     nodes = TreeItem.objects.all()
     return render_to_response('catalog/catalog.html', {'nodes': nodes}, RequestContext(request))
@@ -54,11 +50,64 @@ def section_details(request, model, slug):
 #===============================================================================
 
 def item_view(request, model, slug=None, object_id=None):
+    '''
+    Render catalog page for object
+    
+    Url variables:
+        model:
+            mdoel name
+        slug:
+            object's slug
+        object_id
+            object's id
+    
+    Required at least one of ``slug`` or ``object_id`` parameters
+    
+    Templates:
+        'catalog/<app_label>/<model_name>.html'
+        
+        'catalog/<model_name>.html'
+        
+        '<app_label>/<model_name>_in_catalog.html'
+        
+        'catalog/treeitem.html',
+    
+    Context taken from ``object_list`` method from ``django.views.generic.list_detail``:
+        object_list
+            list of objects
+        is_paginated
+            are the results paginated?
+        results_per_page
+            number of objects per page (if paginated)
+        has_next
+            is there a next page?
+        has_previous
+            is there a prev page?
+        page
+            the current page
+        next
+            the next page
+        previous
+            the previous page
+        pages
+            number of pages, total
+        hits
+            number of objects, total
+        last_on_page
+            the result number of the last of object in the
+            object_list (1-indexed)
+        first_on_page
+            the result number of the first object in the
+            object_list (1-indexed)
+        page_range:
+            A list of the page numbers (1-indexed).
+    
+    '''
     ModelClass = None
     for model_cls in connected_models():
         if model_cls._meta.module_name == model:
             ModelClass = model_cls
-    
+
     if ModelClass is not None:
         # select template
         try:
@@ -67,6 +116,7 @@ def item_view(request, model, slug=None, object_id=None):
                 'catalog/%s/%s.html' % (opts.app_label, opts.module_name),
                 'catalog/%s.html' % opts.module_name,
                 '%s/%s_in_catalog.html' % (opts.app_label, opts.module_name),
+                'catalog/treeitem.html',
             ])
             defaults.update({
                 'template_name': t.name,
@@ -84,5 +134,56 @@ def item_view(request, model, slug=None, object_id=None):
         return HttpResponseNotFound(_('Model %s does not registered' % model))
 
 def root(request):
+    '''
+    Render catalog root page.
+    
+    Templates:
+        ``<app_label>/catalog_root.html``
+        
+        ``catalog/catalog_root.html``
+    
+    Parameter ``app_label`` looked up in ``CATALOG_MDOELS`` setting in settings.py
+    
+    Context taken from ``object_detail`` method from ``django.views.generic.list_detail``:
+        object_list:
+            list of objects
+        is_paginated:
+            are the results paginated?
+        results_per_page:
+            number of objects per page (if paginated)
+        has_next:
+            is there a next page?
+        has_previous:
+            is there a prev page?
+        page:
+            the current page
+        next:
+            the next page
+        previous:
+            the previous page
+        pages:
+            number of pages, total
+        hits:
+            number of objects, total
+        last_on_page:
+            the result number of the last of object in the
+            object_list (1-indexed)
+        first_on_page:
+            the result number of the first object in the
+            object_list (1-indexed)
+        page_range:
+            A list of the page numbers (1-indexed).
+        
+    
+    '''
+    t = loader.select_template([
+        'catalog/%s/%s.html' % (opts.app_label, opts.module_name),
+        'catalog/%s.html' % opts.module_name,
+        '%s/%s_in_catalog.html' % (opts.app_label, opts.module_name),
+    ])
+    defaults.update({
+        'template_name': t.name,
+    })
+
     return object_list(request, TreeItem.objects.filter(parent=None),
-        template_name='catalog/root.html')
+        **defaults)
