60204399ac517040e86916cf1e86c1c34e65d66e
Ivan Gromov <ivan.gromov@redsolution.ru>
2011-05-30 17:53:48 +0600

    Removed old views, fixed errors in few new

--- catalog/views.py
+++ catalog/views.py
@@ -1,53 +1,10 @@
 # -*- coding: utf-8 -*-
-from catalog import settings as catalog_settings
 from catalog.models import TreeItem
-from catalog.utils import connected_models
-from django.db.models import loading
-from django.http import Http404, HttpResponseNotFound
-from django.shortcuts import render_to_response
-from django.template import RequestContext, loader
+from catalog.utils import connected_models, get_data_appnames
+from django.http import HttpResponseNotFound
+from django.template import loader
 from django.views.generic.list_detail import object_detail, object_list
 
-def catalog(request):
-    nodes = TreeItem.objects.all()
-    return render_to_response('catalog/catalog.html', {'nodes': nodes}, RequestContext(request))
-
-def item_details(request, model, slug):
-    models = {}
-    for app_label, model_name in catalog_settings.CATALOG_MODELS:
-        m = loading.cache.get_model(app_label, model_name)
-        models[m.__name__.lower()] = m
-    
-    ModelClass = models[model]
-    object = ModelClass.objects.get(slug=slug)
-    item = TreeItem.objects.get(object_id=object.id, content_type__model=model)
-    
-    return render_to_response('catalog/item.html', {'item': item}, RequestContext(request))
-
-def section_details(request, model, slug):
-    models = {}
-    for app_label, model_name in catalog_settings.CATALOG_MODELS:
-        m = loading.cache.get_model(app_label, model_name)
-        models[m.__name__.lower()] = m
-    
-    ModelClass = models[model]
-    object = ModelClass.objects.get(slug=slug)
-    node = TreeItem.objects.get(object_id=object.id, content_type__model=model)
-    children = node.get_children()
-    
-    sections = []
-    items = []
-    for child in children:
-        if child.content_object.leaf:
-            items.append(child)
-        else:
-            sections.append(child)
-
-    return render_to_response('catalog/section.html', {'items': items, 'sections': sections, 'cur_node': node}, RequestContext(request))
-
-#===============================================================================
-# I will leave onlu these views, all views above should be deleted
-#===============================================================================
 
 def item_view(request, model, slug=None, object_id=None):
     '''
@@ -118,15 +75,15 @@ def item_view(request, model, slug=None, object_id=None):
                 '%s/%s_in_catalog.html' % (opts.app_label, opts.module_name),
                 'catalog/treeitem.html',
             ])
-            defaults.update({
+            extra_context = {
                 'template_name': t.name,
-            })
+            }
         except loader.TemplateDoesNotExist:
             pass
         if slug is not None:
-            return object_detail(request, ModelClass.objects.all(), slug=slug, **defaults)
+            return object_detail(request, ModelClass.objects.all(), slug=slug, **extra_context)
         elif id is not None:
-            return object_detail(request, ModelClass.objects.all(), object_id=object_id, **defaults)
+            return object_detail(request, ModelClass.objects.all(), object_id=object_id, **extra_context)
         else:
             return HttpResponseNotFound(_('No object data specified'))
 
@@ -173,17 +130,14 @@ def root(request):
             object_list (1-indexed)
         page_range:
             A list of the page numbers (1-indexed).
-        
-    
-    '''
-    t = loader.select_template([
-        'catalog/%s/%s.html' % (opts.app_label, opts.module_name),
-        'catalog/%s.html' % opts.module_name,
-        '%s/%s_in_catalog.html' % (opts.app_label, opts.module_name),
-    ])
-    defaults.update({
-        'template_name': t.name,
-    })
 
-    return object_list(request, TreeItem.objects.filter(parent=None),
-        **defaults)
+    '''
+
+    templates_list = ['%s/catalog_root.html' % app_name for app_name in get_data_appnames()]
+    templates_list.append('catalog/root.html')
+    t = loader.select_template(templates_list)
+    extra_context = {
+        'template_name': t.name,
+    }
+
+    return object_list(request, TreeItem.objects.filter(parent=None), **extra_context)
