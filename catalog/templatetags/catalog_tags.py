5c6ffef924e90193ce05aea77d8ea3d059b8fe8a
Ivan Gromov <ivan.gromov@redsolution.ru>
2011-05-30 17:26:01 +0600

    Added children tag

---
+++ catalog/templatetags/catalog_tags.py
@@ -0,0 +1,62 @@
+from classytags.arguments import Argument
+from classytags.core import Tag, Options
+from django import template
+from django.template import loader
+from django.template.loader import render_to_string
+from catalog.utils import get_data_appnames
+
+register = template.Library()
+
+class CatalogChildren(Tag):
+    '''
+    Render or get chlidren for given object. Object must be registered in 
+    catalog tree and have the ``tree()`` attribute.
+    
+    Usage::
+    
+        {% catalog_children for my_section [type children_type] [as varname] %}
+    
+    Examples::
+    
+        {% catalog_children for my_section %}
+    
+    Render children for ``my_section`` context object. ::
+
+        {% catalog_children for my_section as children %}
+    
+    Render children for ``my_section`` into ``children`` context variable. ::
+    
+            {% catalog_children for my_section type item %}
+    
+    Render children only with type ``item`` for ``my_section``.
+    
+        
+        
+    '''
+    name = 'catalog_children'
+    templates = ['catalog/children_tag.html', ]
+
+    options = Options(
+        'for',
+        Argument('object'),
+        'type',
+        Argument('children_type', required=False, resolve=False),
+        'as',
+        Argument('varname', required=False, resolve=False)
+    )
+
+    def render_tag(self, context, object, children_type, varname):
+        treeitem = object.tree.get()
+        children_qs = treeitem.children.all()
+        if children_type:
+            children_qs = children_qs.filter(content_type__model=children_type)
+
+        if varname:
+            context[varname] = children_qs
+            return ''
+        else:
+            self.templates[0:0] = ['%s/children_tag.html' % app_name for app_name in get_data_appnames()]
+            context['children_queryset'] = children_qs
+            return render_to_string(self.templates, context)
+
+register.tag(CatalogChildren)
