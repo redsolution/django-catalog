3d862d04dccf1d26510ce9b4ad387810f7f6877b
Ivan Gromov <ivan.gromov@redsolution.ru>
2011-06-01 15:24:49 +0600

    Added more guessing magic for tags

--- catalog/templatetags/catalog_tags.py
+++ catalog/templatetags/catalog_tags.py
@@ -1,11 +1,11 @@
+from catalog.models import TreeItem
+from catalog.utils import get_data_appnames
 from classytags.arguments import Argument, ChoiceArgument
 from classytags.core import Tag, Options
 from classytags.helpers import InclusionTag
 from django import template
-from django.template import loader
+from django.template import loader, TemplateSyntaxError
 from django.template.loader import render_to_string
-from catalog.utils import get_data_appnames
-from catalog.models import TreeItem
 from django.utils.translation import ugettext_lazy as _
 
 register = template.Library()
@@ -15,47 +15,84 @@ TREE_TYPE_COLLAPSED = 'collapsed'
 TREE_TYPE_DRILLDOWN = 'drilldown'
 
 
+def get_treeitem_from_context(context, silent=True):
+    '''
+    Utility tries to get TreItem from ``context['object']``.
+    
+    If silent=False, returns TreeItem instance or raises TemplateSyntaxError,
+    otherwise returns TreeItem instance or None
+    '''
+    # Try to resolve ``object`` from context
+    if 'object' in context and (getattr(context['object'], 'tree'), None):
+        obj = context['object']
+        if (hasattr(obj.tree, 'get') and callable(obj.tree.get)):
+            # Check that object.tree.get() returns TreeItem instance
+            if (isinstance(context['object'].tree.get(), TreeItem)):
+                treeitem = obj.tree.get()
+    if silent:
+        return None
+    else:
+        raise TemplateSyntaxError('No TreeItem instance found in context')
+
 class CatalogChildren(Tag):
     '''
     Render or get chlidren for given object. Object must be registered in 
     catalog tree and have the ``tree()`` attribute.
     
-    Usage::
+    **Usage**::
     
-        {% catalog_children for my_section [type children_type] [as varname] %}
-    
-    Examples::
-    
-        {% catalog_children for my_section %}
-    
-    Render children for ``my_section`` context object. ::
+        {% catalog_children [for my_section] [type children_type] [as varname] %}
 
-        {% catalog_children for my_section as children %}
+    **Magic**
     
-    Render children for ``my_section`` into ``children`` context variable. ::
+        If context has ``object`` variable and *for* paramenter is not specified,
+        it will try to fetch ``object.tree.get()`` automatically, and if it find
+        ``TreeItem`` instance as result of object.tree.get(), it will 
+        show children for it.
     
+    **Examples**
+        
+        1. Render children for ``my_section`` context object. ::
+           
+            {% catalog_children for my_section %}
+            
+          or render children for ``object`` in context ::
+          
+            {% catalog_children for my_section %}
+        
+        2. Render children for ``my_section`` into ``children`` context variable. ::
+    
+            {% catalog_children for my_section as children %}
+        
+        3. Render children only with type ``item`` for ``my_section`` ::
+        
             {% catalog_children for my_section type item %}
-    
-    Render children only with type ``item`` for ``my_section``.
-    
         
-        
+
     '''
     name = 'catalog_children'
     templates = ['catalog/children_tag.html', ]
 
     options = Options(
         'for',
-        Argument('object'),
+        Argument('instance', required=False),
         'type',
         Argument('children_type', required=False, resolve=False),
         'as',
         Argument('varname', required=False, resolve=False)
     )
 
-    def render_tag(self, context, object, children_type, varname):
-        treeitem = object.tree.get()
-        children_qs = treeitem.children.published()
+    def render_tag(self, context, instance, children_type, varname):
+        if instance is not None:
+            try:
+                treeitem = instance.tree.get()
+            except AttributeError:
+                raise TemplateSyntaxError('Instance argument must have `tree` attribute')
+        else:
+            # try to guess
+            treeitem = get_treeitem_from_context(context)
+
+        children_qs = TreeItem.objects.published().filter(parent=treeitem)
         if children_type:
             children_qs = children_qs.filter(content_type__model=children_type)
 
@@ -71,21 +108,19 @@ register.tag(CatalogChildren)
 
 
 class BreadcrumbTag(InclusionTag):
+    '''
+    TODO: DOCME!
+    '''
     name = 'catalog_breadcrumbs'
     template = 'catalog/breadcrumbs.html'
 
     def get_context(self, context, **kwargs):
-        # Try to resolve ``object`` from context
-        if 'object' in context and (getattr(context['object'], 'tree'), None):
-            obj = context['object']
-            if (hasattr(obj.tree, 'get') and callable(obj.tree.get)):
-                # Check that object.tree.get() returns TreeItem instance
-                if (isinstance(context['object'].tree.get(), TreeItem)):
-                     treeitem = obj.tree.get()
-                     ancestors = list(treeitem.get_ancestors())
-
-                     return {'breadcrumbs': ancestors + [treeitem, ] }
-        return {}
+        treeitem = get_treeitem_from_context(context, silent=False)
+        if treeitem is not None:
+            ancestors = list(treeitem.get_ancestors())
+            return {'breadcrumbs': ancestors + [treeitem, ] }
+        else:
+            return {}
 
 register.tag(BreadcrumbTag)
 
@@ -160,12 +195,7 @@ class CatalogTree(Tag):
             children = TreeItem.objects.published().filter(parent=None)
         if active is None:
             # Try to resolve ``object`` from context
-            if 'object' in context and (getattr(context['object'], 'tree'), None):
-                obj = context['object']
-                if (hasattr(obj.tree, 'get') and callable(obj.tree.get)):
-                    # Check that object.tree.get() returns TreeItem instance
-                    if (isinstance(context['object'].tree.get(), TreeItem)):
-                        active = obj.tree.get()
+            active = get_treeitem_from_context(context)
 
         if active is not None:
             context['breadcrumbs'] = [active]
